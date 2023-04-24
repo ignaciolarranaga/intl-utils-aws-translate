c0d1010ee50569a7fa7f880733f6fbc927628258
ignaciolarranaga <ignaciolarranaga@gmail.com>
2023-04-24 18:20:00 -0300

    Adjustments for prettier

--- src/index.ts
+++ src/index.ts
@@ -15,31 +15,40 @@ const LANGUAGES = [ 'af', 'sq', 'am', 'ar', 'hy',  'az', 'bn', 'bs', 'bg',
 ]
 
 const fromOption = new Option(
-    '-f, --from [language]',
-    'the language we are translating from'
-  )
+  '-f, --from [language]',
+  'the language we are translating from'
+)
   .choices(LANGUAGES)
   .makeOptionMandatory()
-const toOption = new Option('-t, --to [languages...]', 'the languages to translate to')
+const toOption = new Option(
+  '-t, --to [languages...]',
+  'the languages to translate to'
+)
   .choices(LANGUAGES)
   .makeOptionMandatory()
 
 program
   .name('intl-util-aws-translate')
-  .description('Generates or populates the translation files based on AWS Translate. ' +
-  'See supported languages in here: https://docs.aws.amazon.com/translate/latest/dg/what-is-languages.html')
+  .description(
+    'Generates or populates the translation files based on AWS Translate. ' +
+      'See supported languages in here: https://docs.aws.amazon.com/translate/latest/dg/what-is-languages.html'
+  )
   .version('1.0.0')
   .option('--no-unchanged', 'Does not logs unchanged files')
-  .option('-p, --pattern [pattern]', 'the pattern of the files', '**/*.translations.js')
+  .option(
+    '-p, --pattern [pattern]',
+    'the pattern of the files',
+    '**/*.translations.js'
+  )
   .addOption(fromOption)
   .addOption(toOption)
 
 program.parse()
-const opts = program.opts();
-const translateFrom = opts.from;
-const translateTo = opts.to;
-const pattern = opts.pattern;
-const logUnchanged = opts.unchanged;
+const opts = program.opts()
+const translateFrom = opts.from
+const translateTo = opts.to
+const pattern = opts.pattern
+const logUnchanged = opts.unchanged
 
 const translate = new Translate({})
 
@@ -56,16 +65,16 @@ interface Job {
  * @returns An array containing all the keys to translate
  */
 function calculateKeys(definition: Translations) {
-  const keys = [];
+  const keys = []
   for (const language of translateTo) {
     for (const key in definition[language]) {
       if (keys.indexOf(key) === -1) {
-        keys.push(key);
+        keys.push(key)
       }
     }
   }
 
-  return keys;
+  return keys
 }
 
 /**
@@ -73,21 +82,27 @@ function calculateKeys(definition: Translations) {
  * @param fileName The name of the file to process
  */
 function translateFile(fileName: string) {
-  let file = require(fileName);
-  let definition = fileName.endsWith('.ts') ? file.default : file;
+  let file = require(fileName)
+  let definition = fileName.endsWith('.ts') ? file.default : file
 
   const jobs: Job[] = []
-  const keys = calculateKeys(definition);
+  const keys = calculateKeys(definition)
 
   for (const TargetLanguageCode of translateTo) {
     for (const key of keys) {
-      if (!definition[TargetLanguageCode] || !definition[TargetLanguageCode][key]) {
+      if (
+        !definition[TargetLanguageCode] ||
+        !definition[TargetLanguageCode][key]
+      ) {
         jobs.push({
           SourceLanguageCode: translateFrom,
           TargetLanguageCode,
           OriginalText: key,
           // When there is translation in the from language is because we want to replace the original text
-          Text: definition[translateFrom] && definition[translateFrom][key] ? definition[translateFrom][key] : key,
+          Text:
+            definition[translateFrom] && definition[translateFrom][key]
+              ? definition[translateFrom][key]
+              : key,
         })
       }
     }
@@ -105,15 +120,19 @@ function translateFile(fileName: string) {
           results[i].TranslatedText
       }
 
-      let contents;
+      let contents
       if (fileName.endsWith('.js')) {
-        contents = '/* spellchecker: disable */\n' +
-          `module.exports = ${JSON.stringify(definition, undefined, 2)}\n`;
+        contents =
+          '/* prettier-ignore */\n' +
+          '/* spellchecker: disable */\n' +
+          `module.exports = ${JSON.stringify(definition, undefined, 2)}\n`
       } else if (fileName.endsWith('.ts')) {
-        contents = '/* spellchecker: disable */\n' +
-          `export default ${JSON.stringify(definition, undefined, 2)}\n`;
+        contents =
+          '/* prettier-ignore */\n' +
+          '/* spellchecker: disable */\n' +
+          `export default ${JSON.stringify(definition, undefined, 2)}\n`
       } else {
-        contents = JSON.stringify(definition, undefined, 2) + '\n';
+        contents = JSON.stringify(definition, undefined, 2) + '\n'
       }
 
       fs.writeFile(fileName, contents, () => {
